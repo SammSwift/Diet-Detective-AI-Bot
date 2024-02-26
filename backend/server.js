37f46767acf3b3d230e633f5cee43a8462501c09
SammSwift <samyyswift@gmail.com>
2024-02-26 21:12:45 +0100

    made slight changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import path from "path";
 const PORT = 5000;
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" })); // Body parser middleware
+app.use(cors({ origin: "*" })); // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,6 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "develeopment" ? "http://localhost:5000" : "";
+  process.env.NODE_ENV === "develeopment"
+    ? "http://localhost:5000"
+    : "https://diet-detector-ai-bot.onrender.com";
 export const AI_URL = "/api/ai_bot";
 export const UPLOAD_URL = "/api/upload";
