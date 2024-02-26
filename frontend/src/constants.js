b4fa12ae968a9ea457813fe909eecada6e861550
SammSwift <samyyswift@gmail.com>
2024-02-26 23:02:17 +0100

    updated base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 export const AI_URL = "/api/ai-bot";
 export const UPLOAD_URL = "/api/upload";
