bfbff102008c14401dbfe187d35b6424b3158724
SammSwift <samyyswift@gmail.com>
2024-02-27 10:47:08 +0100

    added upload folder

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,6 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.use("/uploads", express.static("/uploads"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
